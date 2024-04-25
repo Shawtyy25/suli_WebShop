27cb7d955cc38973b5c765e92cefd72f8de7f8bb
shawtyy <szolnokiabi25@gmail.com>
2024-04-25 18:57:30 +0200

    dark theme light theme start

--- Main2/main2.js
+++ Main2/main2.js
@@ -1,9 +1,9 @@
-function mouseOverEffects(currentDiv, randWindow, randWindow2, currentWindow, randomWindow3) {
+function mouseEffects(currentDiv, randWindow, randWindow2, currentWindow, randomWindow3) {
     currentDiv.addEventListener('mouseover', () => {
-      randWindow.style.display = ''
-      randWindow2.style.display = ''
-      randomWindow3.style.display = ''
-      currentWindow.style.display = 'flex'  
+        randWindow.style.display = ''
+        randWindow2.style.display = ''
+        randomWindow3.style.display = ''
+        currentWindow.style.display = 'flex'
     })
 
     currentWindow.addEventListener('mouseover', () => {
@@ -16,6 +16,33 @@ function mouseOverEffects(currentDiv, randWindow, randWindow2, currentWindow, ra
 }
 
 
+function darkTheme(body, dropdown, arrowUp){
+    dropdown.forEach(item => {
+        item.style.backgroundColor = 'var(--darkNav)'    
+    });
+    
+    body.style.backgroundColor = 'var(--darkBg)';
+    arrowUp.forEach(item => {
+        item.style.color = 'var(--darkNav)';
+    })
+    
+    body.style.transition = 'all .3s'
+}
+
+
+function lightTheme(body, dropdown, arrowUp){
+    dropdown.forEach(item => {
+        item.style.backgroundColor = 'var(--lightNav)'    
+    });
+    
+    body.style.backgroundColor = 'var(--lightBg)';
+    arrowUp.forEach(item => {
+        item.style.color = 'var(--lightNav)';
+    })
+
+    body.style.transition = 'all .3s'
+}
+
 const loginData_value = document.querySelector('.loginData h4')
 const loginForm = document.querySelector('.loginForm')
 const loginDiv = document.querySelector('.loginDiv')
@@ -26,9 +53,18 @@ const favourites = document.querySelector('.favourites')
 const favouritesWindow = document.querySelector('.favouritesWindow')
 const cart = document.querySelector('.cart')
 const cartWindow = document.querySelector('.cartWindow')
+const themeChange = document.querySelector('.themeChange')
+const lightT_Div = document.querySelector('.themeLight')
+const darkT_Div = document.querySelector('.themeDark')
+const close_Themes = document.querySelector('.closeWindow')
+const t_Settings = document.querySelector('.t_Settings')
+const body = document.querySelector('body')
+const dropdown_window = document.querySelectorAll('.dropdown_window')
+const arrowUp = document.querySelectorAll('.arrowUp_2')
+const searchBar = document.querySelector('#search')
 
 if (loginData_value.textContent == 'Bejelentkezés') {
-    
+
     loginForm.addEventListener('click', () => {
         window.location.href = '../Login/login.html'
     })
@@ -37,8 +73,8 @@ if (loginData_value.textContent == 'Bejelentkezés') {
 if (localStorage.length > 0) {
     let user_Data = localStorage.getItem('login_Username')
     loginData_value.innerHTML = '<h4>' + user_Data + '</h4>'
-    
-    mouseOverEffects(loginDiv, favouritesWindow, loginWindow, afterLoginWindow, cartWindow)
+
+    mouseEffects(loginDiv, favouritesWindow, loginWindow, afterLoginWindow, cartWindow)
 
     logout.addEventListener('click', () => {
         window.location.href = '../Login/login.html'
@@ -46,8 +82,34 @@ if (localStorage.length > 0) {
 }
 
 if (localStorage.length == 0) {
-    mouseOverEffects(loginDiv, favouritesWindow, afterLoginWindow, loginWindow, cartWindow)
+    mouseEffects(loginDiv, favouritesWindow, afterLoginWindow, loginWindow, cartWindow)
 }
 
-mouseOverEffects(favourites, loginWindow, afterLoginWindow, favouritesWindow, cartWindow)
+mouseEffects(favourites, loginWindow, afterLoginWindow, favouritesWindow, cartWindow)
+mouseEffects(cart, loginWindow, afterLoginWindow, cartWindow, favouritesWindow)
+
+
+
+close_Themes.addEventListener('click', () => {
+    themeChange.style.display = ''
+})
+
+t_Settings.addEventListener('click', () => {
+    themeChange.style.display = 'flex'
+})
+
+
+
+darkT_Div.addEventListener('click', () => {
+    darkTheme(body, dropdown_window, arrowUp)
+    themeChange.style.display = ''
+})
+
+lightT_Div.addEventListener('click', () => {
+    lightTheme(body, dropdown_window, arrowUp)
+    themeChange.style.display = ''
+})
+
+
+
 
