291a6b69c692bf5586385e9a8c07e9ba2fb29772
shawtyy <szolnokiabi25@gmail.com>
2024-04-25 22:35:32 +0200

    hoverTry settings--

--- Main2/main2.js
+++ Main2/main2.js
@@ -43,6 +43,8 @@ function lightTheme(body, dropdown, arrowUp){
     body.style.transition = 'all .3s'
 }
 
+
+
 const loginData_value = document.querySelector('.loginData h4')
 const loginForm = document.querySelector('.loginForm')
 const loginDiv = document.querySelector('.loginDiv')
@@ -62,6 +64,15 @@ const body = document.querySelector('body')
 const dropdown_window = document.querySelectorAll('.dropdown_window')
 const arrowUp = document.querySelectorAll('.arrowUp_2')
 const searchBar = document.querySelector('#search')
+const closeWindowSettings = document.querySelector('.closeWindowSettings')
+const settingsDiv = document.querySelector('.settingsDiv')
+const settings = document.querySelector('.Settings')
+const defaultV = document.querySelector('.default')
+const user = document.querySelector('.user')
+const rate = document.querySelector('.rate')
+const messege = document.querySelector('.messege')
+const settings_a = document.querySelectorAll('.settings_a')
+
 
 if (loginData_value.textContent == 'Bejelentkezés') {
 
@@ -96,6 +107,7 @@ close_Themes.addEventListener('click', () => {
 
 t_Settings.addEventListener('click', () => {
     themeChange.style.display = 'flex'
+    afterLoginWindow.style.display = ''
 })
 
 
@@ -110,6 +122,19 @@ lightT_Div.addEventListener('click', () => {
     themeChange.style.display = ''
 })
 
+closeWindowSettings.addEventListener('click', () => {
+    settingsDiv.style.transition = 'all .4s'
+    settingsDiv.style.display = ''
+})
 
+settings.addEventListener('click', () => {
+    settingsDiv.style.display = 'flex'
+    afterLoginWindow.style.display = ''
+})
 
-
+settings_a.forEach(items => {
+    items.addEventListener('click', () => {
+        items.style.backgroundColor = 'rgb(202, 200, 200)'
+    })
+    
+})
